fd8e9783aea7bc4cd71e181e756f56fd5928df53
zierejo1 <jonas.zierer@novartis.com>
2019-08-21 08:33:39 -0400

    fix gene annotation for long genes

--- R/singlePlotRACER.R
+++ R/singlePlotRACER.R
@@ -38,172 +38,191 @@ singlePlotRACER <- function(assoc_data, chr,
                             label_lead = FALSE,
                             textsize = 8){
 
-  if(missing(assoc_data)){
-    stop("Please provide a data set to plot.")
-  }else if(missing(chr)){
-    stop("Please specify which chromosome you wish to plot.")
-  }else if(missing(plotby)){
-    stop("Please specify the method by which you wish to plot.")
-  }else if(plotby == "gene"){
-    if(is.null(gene_plot)){
-      stop("Please specify a gene to plot by.")
+    if(missing(assoc_data)){
+        stop("Please provide a data set to plot.")
+    }else if(missing(chr)){
+        stop("Please specify which chromosome you wish to plot.")
+    }else if(missing(plotby)){
+        stop("Please specify the method by which you wish to plot.")
+    }else if(plotby == "gene"){
+        if(is.null(gene_plot)){
+            stop("Please specify a gene to plot by.")
+        }
+    }else if(plotby == "snp"){
+        if(is.null(snp_plot)){
+            stop("Please specify a snp to plot by.")
+        }
+    }else if(plotby == "coord"){
+        if(is.null(start_plot) | is.null(end_plot)){
+            stop("Please specify start coordinate for plot.")
+        }
+    }else{
+        message("All inputs are go.")
     }
-  }else if(plotby == "snp"){
-    if(is.null(snp_plot)){
-      stop("Please specify a snp to plot by.")
+
+    reqs = c("CHR", "POS", "LOG10P")
+    cols = colnames(assoc_data)
+    if(sum(reqs %in% cols) == 3){
+    }else{stop("Association Data Set is missing a required column, please format your data set using formatRACER.R.")}
+
+    reqs_2 = c("LD", "LD_BIN")
+    if(sum(reqs_2 %in% cols) == 2){
+    }else{message("Association Data Set is missing LD data, the resulting plot won't have LD information, but you can add it using the ldRACER.R function.")}
+
+    `%>%` <- magrittr::`%>%`
+
+    ## GET GENE ANNOTATION
+    message("gene annotation")
+    if(build == "hg38"){
+        utils::data(hg38)
+        colnames(hg38) = c("GENE_ID", "CHR", "TRX_START", "TRX_END", "LENGTH", "GENE_NAME", "TYPE")
+        gene_sub = hg38
+    }else if(build == "hg19"){
+        utils::data(hg19)
+        colnames(hg19) = c("GENE_ID", "CHR", "TRX_START", "TRX_END", "LENGTH", "GENE_NAME", "TYPE")
+        gene_sub = hg19
     }
-  }else if(plotby == "coord"){
-    if(is.null(start_plot) | is.null(end_plot)){
-      stop("Please specify start coordinate for plot.")
+    if(set == "protein_coding"){
+        gene_sub = gene_sub[gene_sub$TYPE == "protein_coding",]
+    }else{
+        gene_sub = gene_sub
     }
-  }else{
-    message("All inputs are go.")
-  }
 
-  reqs = c("CHR", "POS", "LOG10P")
-  cols = colnames(assoc_data)
-  if(sum(reqs %in% cols) == 3){
-  }else{stop("Association Data Set is missing a required column, please format your data set using formatRACER.R.")}
-
-  reqs_2 = c("LD", "LD_BIN")
-  if(sum(reqs_2 %in% cols) == 2){
-  }else{message("Association Data Set is missing LD data, the resulting plot won't have LD information, but you can add it using the ldRACER.R function.")}
-
-  `%>%` <- magrittr::`%>%`
-
-  if(build == "hg38"){
-    utils::data(hg38)
-    chr_in = chr
-    colnames(hg38) = c("GENE_ID", "CHR", "TRX_START", "TRX_END", "LENGTH", "GENE_NAME", "TYPE")
-    gene_sub = hg38[hg38$CHR == chr_in,]
-  }else if(build == "hg19"){
-    utils::data(hg19)
-    chr_in = chr
-    colnames(hg19) = c("GENE_ID", "CHR", "TRX_START", "TRX_END", "LENGTH", "GENE_NAME", "TYPE")
-    gene_sub = hg19[hg19$CHR == chr_in,]
-  }
-
-  if(set == "protein_coding"){
-    gene_sub = gene_sub[gene_sub$TYPE == "protein_coding",]
-  }else{
-    gene_sub = gene_sub
-  }
-
-  if(sum(is.null(plotby)) == 1){
-    stop("Please specify a method by which to plot.")
-  }
-
-  if(sum(is.null(plotby)) == 0){
-    message("Plotting by...")
-    if((plotby == "coord") == TRUE){
-      message("coord")
-      start = start_plot
-      end = end_plot
-    }else if((plotby == "gene") == TRUE){
-      message(paste("gene:",gene_plot))
-      if(sum(is.null(gene_plot)) == 0){
-        p = subset(gene_sub, gene_sub$GENE_NAME == gene_plot)
-        start = min(p$TRX_START) - 500000
-        end = max(p$TRX_END) + 500000
-      }else{message("No gene specified.")}
-    }else if((plotby == "snp") == TRUE){
-      message(paste("snp",snp_plot))
-      q = assoc_data[assoc_data$RS_ID == snp_plot,]
-      w = q$POS
-      w = as.numeric(as.character(w))
-      start = w - 500000
-      end = w + 500000}
-  }
-
-  # reading in gene data
-  gene_sub = subset(gene_sub, gene_sub$TRX_START > (start-50000))
-  gene_sub = subset(gene_sub, gene_sub$TRX_END < (end+50000))
-  gene_sub = gene_sub[,c(3,4,6)]
-  gene_sub = reshape2::melt(gene_sub,id.vars = "GENE_NAME")
-  gene_sub$y_value = as.numeric(as.factor(gene_sub$GENE_NAME))
-  plot_lab = subset(gene_sub, gene_sub$variable == "TRX_END")
-
-  # read in, format, and filter data sets
-  message("Reading in association data")
-  in.dt <- as.data.frame(assoc_data)
-  in.dt$POS = as.numeric(as.character(in.dt$POS))
-  in.dt$LOG10P = as.numeric(as.character(in.dt$LOG10P))
-  in.dt$CHR = as.numeric(as.character(in.dt$CHR))
-  in.dt = dplyr::filter(in.dt, .data$CHR == chr_in)
-  in.dt = dplyr::filter(in.dt, .data$POS > start) %>%
-    dplyr::filter(.data$POS < end)
-
-  if(label_lead == TRUE){
-    lsnp_row = which(in.dt$LABEL == "LEAD")
-    label_data = in.dt[lsnp_row,]
-    if(dim(label_data)[1] == 0){
-      lsnp_row = in.dt[in.dt$LOG10P == max(in.dt$LOG10P),]
-      label_data = lsnp_row[1,]
+    ## POSITIONS
+    if(sum(is.null(plotby)) == 1){
+        stop("Please specify a method by which to plot.")
     }
-  }
-  # Generate plots
-  message("Generating Plot")
-  if("LD" %in% colnames(in.dt) && "LD_BIN" %in% colnames(in.dt)){
-    c = ggplot2::ggplot(gene_sub, ggplot2::aes_string(x = "value", y = "y_value")) +
-        ggplot2::geom_line(ggplot2::aes_string(group = "GENE_NAME"), size = 2) + ggplot2::theme_bw() +
-        ggplot2::geom_text(data = plot_lab, ggplot2::aes(x = value, y = y_value, label = GENE_NAME),
-                           hjust = -0.1,vjust = 0.3, size = textsize/(14/5)*0.8) +
-        ggplot2::theme(axis.title.y = ggplot2::element_text(color = "white", size = 28),
-                       axis.text.y = ggplot2::element_blank(),
-                       axis.ticks.y = ggplot2::element_blank()) +
-        ggplot2::xlab(paste0("Position (chromsome ", chr_in, ")")) +
-        ggplot2::coord_cartesian(xlim = c(start,end), ylim = c(0,(max(gene_sub$y_value)+1))) +
-        ggplot2::theme(text         = ggplot2::element_text(size = textsize),
-                       axis.title   = ggplot2::element_text(face = "bold"),
-                       legend.title = ggplot2::element_text(face = "bold"),
-                       plot.margin  = ggplot2::unit(c(0,0,0,0), "cm"))
+    if(sum(is.null(plotby)) == 0){
+        message("Plotting by...")
+        if((plotby == "coord") == TRUE){
+            message("coord")
+            start = start_plot
+            end = end_plot
+        }else if((plotby == "gene") == TRUE){
+            message(paste("gene:",gene_plot))
+            if(sum(is.null(gene_plot)) == 0){
+                p = subset(gene_sub, gene_sub$GENE_NAME == gene_plot)
+                start = min(p$TRX_START) - 500000
+                end = max(p$TRX_END) + 500000
+            }else{message("No gene specified.")}
+        }else if((plotby == "snp") == TRUE){
+            message(paste("snp",snp_plot))
+            q = assoc_data[assoc_data$RS_ID == snp_plot,]
+            w = q$POS
+            w = as.numeric(as.character(w))
+            start = w - 500000
+            end = w + 500000}
+    }
+    
+    ## reading in gene data
+    message("format gene labels")
+    gene_sub <- gene_sub %>%
+        filter(CHR == chr) %>%
+        filter(pmax(TRX_START, start) <= pmin(TRX_END, end)) %>%
+        select(TRX_START, TRX_END, GENE_NAME) %>%
+        mutate(TRX_END = as.numeric(TRX_END),
+               TRX_START = as.numeric(TRX_START)) %>%
+        mutate(lab   = case_when(
+                   TRX_END   < end   ~ TRX_END,
+                   TRX_START > start ~ TRX_START,
+                   TRUE              ~ end),
+               hjust = case_when(
+                   TRX_END   < end   ~ -0.1,
+                   TRX_START > start ~ 1.1,
+                   TRUE              ~ 1)) %>%
+        gather(variable, value, TRX_START, TRX_END)
 
-    b = ggplot2::ggplot(in.dt, ggplot2::aes_string(x = "POS", y = "LOG10P", color = "LD_BIN")) +
-        ggplot2::geom_point() +
-        ggplot2::scale_colour_manual(
-                     values = c("1.0-0.8" = "red", "0.8-0.6" = "darkorange1", "0.6-0.4" = "green1",
-                                "0.4-0.2" = "skyblue1", "0.2-0.0" = "navyblue", "NA" = "grey"), drop = FALSE) +
-        ggplot2::theme_bw() +
-        ggplot2::labs(x      = NULL,
-                      y      = "-log10(p-value)",
-                      colour = bquote(R^2)) +
-        ggplot2::coord_cartesian(xlim = c(start, end),
-                                 ylim = c(min(in.dt$LOG10P),max(in.dt$LOG10P))) +
-        ggplot2::theme(text         = ggplot2::element_text(size = textsize),
-                       axis.title   = ggplot2::element_text(face = "bold"),
-                       legend.title = ggplot2::element_text(face = "bold"),
-                       axis.text.x  = ggplot2::element_blank(),
-                       axis.ticks.x = ggplot2::element_blank(),
-                       plot.margin  = ggplot2::unit(c(0,0,0,0), "cm"))
-                       
+    
+    plot_lab <- gene_sub %>%
+            distinct(GENE_NAME, lab, hjust)
 
-  }else{
-    c = ggplot2::ggplot(gene_sub, ggplot2::aes_string(x = "value", y = "y_value")) +
-      ggplot2::geom_line(ggplot2::aes_string(group = "GENE_NAME"), size = 2) + ggplot2::theme_bw() +
-      ggplot2::geom_text(data = plot_lab, ggplot2::aes_string(x = "value", y = "y_value", label = "GENE_NAME"),
-                         hjust = -0.1,vjust = 0.3, size = 2.5) +
-      ggplot2::theme(axis.title.y = ggplot2::element_text(color = "white", size = 28),
-                     axis.text.y = ggplot2::element_blank(),
-                     axis.ticks.y = ggplot2::element_blank()) + ggplot2::xlab(paste0("Chromosome ", chr_in, " Position")) +
-      ggplot2::coord_cartesian(xlim = c(start,end), ylim = c(0,(max(gene_sub$y_value)+1)))
+                                        # read in, format, and filter data sets
+    message("Reading in association data")
+    in.dt <- as.data.frame(assoc_data) %>%
+        filter(CHR == chr) %>%
+        filter(POS > start) %>%
+        filter(POS < end)
 
-    b = ggplot2::ggplot(in.dt, ggplot2::aes_string(x = "POS", y = "LOG10P")) +
-        ggplot2::geom_point() +
-        ggplot2::theme_bw() +
-        ggplot2::xlab("Chromosome Position") +
-        ggplot2::ylab("-log10(p-value)") +
-        ggplot2::coord_cartesian(xlim = c(start, end),
-                                 ylim = c(min(in.dt$LOG10P),max(in.dt$LOG10P)))
-  }
-  if(label_lead == TRUE){
-      b = b + geom_point(data = label_data,
-                         aes_string(x = "POS", y = "LOG10P"),
-                         color = "purple")
-      b = b + geom_text(data = label_data, aes_string(label = "RS_ID"),
-                        color = "black",
-                        size = textsize/(14/5)*0.8,
-                        hjust = 1.25)
-  }
+    if(label_lead == TRUE){
+        lsnp_row = which(in.dt$LABEL == "LEAD")
+        label_data = in.dt[lsnp_row,]
+        if(dim(label_data)[1] == 0){
+            lsnp_row = in.dt[in.dt$LOG10P == max(in.dt$LOG10P),]
+            label_data = lsnp_row[1,]
+        }
+    }
+    ## Generate plots
+    message("Generating Plot")
+    if("LD" %in% colnames(in.dt) && "LD_BIN" %in% colnames(in.dt)){
+
+        c = ggplot2::ggplot(gene_sub, ggplot2::aes(x = value, y = GENE_NAME)) +
+            ggplot2::geom_line(ggplot2::aes(group = GENE_NAME), size = 2) +
+            ggplot2::theme_bw() +
+            ggplot2::geom_label(data = plot_lab,
+                                ggplot2::aes(x = lab, y = GENE_NAME, label = GENE_NAME,
+                                             hjust = hjust),
+                               size = textsize/(14/5)*0.8, colour = NA) +
+            ggplot2::geom_text(data = plot_lab,
+                               ggplot2::aes(x = lab, y = GENE_NAME, label = GENE_NAME,
+                                             hjust = hjust),
+                               size = textsize/(14/5)*0.8)+
+            ggplot2::xlab(paste0("Position (chromsome ", chr_in, ")")) +
+            ggplot2::coord_cartesian(xlim = c(start,end)) +
+            ggplot2::theme(axis.title         = ggplot2::element_text(face = "bold"),
+                           axis.title.y       = ggplot2::element_blank(),
+                           axis.text.y        = ggplot2::element_blank(),
+                           axis.ticks.y       = ggplot2::element_blank(),
+                           legend.title       = ggplot2::element_text(face = "bold"),
+                           panel.grid.major.y = ggplot2::element_blank(),
+                           panel.grid.minor.y = ggplot2::element_blank(),
+                           plot.margin        = ggplot2::unit(c(0,0,0,0), "cm"))
+
+        b = ggplot2::ggplot(in.dt, ggplot2::aes_string(x = "POS", y = "LOG10P", color = "LD_BIN")) +
+            ggplot2::geom_point() +
+            ggplot2::scale_colour_manual(
+                         values = c("1.0-0.8" = "red", "0.8-0.6" = "darkorange1", "0.6-0.4" = "green1",
+                                    "0.4-0.2" = "skyblue1", "0.2-0.0" = "navyblue", "NA" = "grey"), drop = FALSE) +
+            ggplot2::theme_bw() +
+            ggplot2::labs(x      = NULL,
+                          y      = "-log10(p-value)",
+                          colour = bquote(R^2)) +
+            ggplot2::coord_cartesian(xlim = c(start, end),
+                                     ylim = c(min(in.dt$LOG10P),max(in.dt$LOG10P))) +
+            ggplot2::theme(text         = ggplot2::element_text(size = textsize),
+                           axis.title   = ggplot2::element_text(face = "bold"),
+                           legend.title = ggplot2::element_text(face = "bold"),
+                           axis.text.x  = ggplot2::element_blank(),
+                           axis.ticks.x = ggplot2::element_blank(),
+                           plot.margin  = ggplot2::unit(c(0,0,0,0), "cm"))
+        
+
+    }else{
+        c = ggplot2::ggplot(gene_sub, ggplot2::aes_string(x = "value", y = "y_value")) +
+            ggplot2::geom_line(ggplot2::aes_string(group = "GENE_NAME"), size = 2) + ggplot2::theme_bw() +
+            ggplot2::geom_text(data = plot_lab, ggplot2::aes_string(x = "value", y = "y_value", label = "GENE_NAME"),
+                               hjust = -0.1,vjust = 0.3, size = 2.5) +
+            ggplot2::theme(axis.title.y = ggplot2::element_text(color = "white", size = 28),
+                           axis.text.y = ggplot2::element_blank(),
+                           axis.ticks.y = ggplot2::element_blank()) + ggplot2::xlab(paste0("Chromosome ", chr_in, " Position")) +
+            ggplot2::coord_cartesian(xlim = c(start,end), ylim = c(0,(max(gene_sub$y_value)+1)))
+
+        b = ggplot2::ggplot(in.dt, ggplot2::aes_string(x = "POS", y = "LOG10P")) +
+            ggplot2::geom_point() +
+            ggplot2::theme_bw() +
+            ggplot2::xlab("Chromosome Position") +
+            ggplot2::ylab("-log10(p-value)") +
+            ggplot2::coord_cartesian(xlim = c(start, end),
+                                     ylim = c(min(in.dt$LOG10P),max(in.dt$LOG10P)))
+    }
+    if(label_lead == TRUE){
+        b = b + geom_point(data = label_data,
+                           aes_string(x = "POS", y = "LOG10P"),
+                           color = "purple")
+        b = b + geom_text(data = label_data, aes_string(label = "RS_ID"),
+                          color = "black",
+                          size = textsize/(14/5)*0.8,
+                          hjust = 1.25)
+    }
     ggpubr::ggarrange(b, c, heights = c(3,1),
                       nrow = 2, ncol = 1,
                       align = "v",
